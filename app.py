6e4981abdc7e38e7f64da3237dcdc45468b3e059
PedroM2626 <pedromoratolahoz@gmail.com>
2025-02-20 10:52:24 -0300

    fixed another bug

--- app.py
+++ app.py
@@ -253,17 +253,21 @@ async def download(interaction: discord.Interaction,
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': '%(title)s.%(ext)s',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': file_format.value,
                 'preferredquality': '192',
             }],
         }
+
     elif file_format.value in ["mp4", "webm"]:
         ydl_opts = {
-            'format': 'bestvideo+bestaudio/best',
-            'outtmpl': '%(title)s.%(ext)s'
+            'format': 'bestaudio/best',
+            'outtmpl': '%(title)s.%(ext)s',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
         }
+
     else:
         await interaction.followup.send("Formato inválido. Use mp3, wav, mp4 ou webm.")
         return
